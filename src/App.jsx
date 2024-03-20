90fddd280c703fa2df1ebfd48027b74288c6a5ce
Ashmit Jain <ashmit27j@gmail.com>
2024-03-20 17:16:19 +0530

    Added login, signup and signup-2 page, added 2 new svgs to images

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import SignUp from './pages/signup'
 import Dashboard from './pages/dashboard'
+import SignUp2 from "./pages/signup-2";
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Router>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Router>
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="/signup-2" element={<SignUp2 />} />
+						<Route path="/dashboard" element={<Dashboard />} />
+					</Routes>
+				</Router>
+			</div>
+		</>
+	);
 }
 
 export default App
--- src/pages/signup-2.jsx
+++ src/pages/signup-2.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Img2 from "../Images/login-img.svg";
+import Img1 from "../Images/signup-img.svg";
 
-const Login = () => {
+const SignUp2 = () => {
 	return (
 		<>
 			<div
 				name="container"
-				className="w-screen h-screen bg-[#19183E] flex flex-col relative justify-end gap-10 content-end text-white font-sans "
+				className="w-screen h-screen bg-[#19183E] flex flex-col relative justify-end content-end text-white font-sans "
 			>
-				<img src={Img2} className="h-[17rem] mx-auto opacity-90"></img>
+				<img
+					src={Img1}
+					className="h-[20rem] w-[20rem] mx-auto opacity-90"
+				></img>
 				<div className=" bg-[#28284C] h-auto w-full lg:w-1/4 p-[2.2rem]  rounded-t-[3rem] font-mainFont">
 					<form action="" name="myForm" className="flex flex-col gap-[1.5rem]">
 						<div className="text-[2rem]">
-							<h1 className="text-center text-white pb-2  ">Login</h1>
+							<h1 className="text-center text-white pb-2  ">SignUp</h1>
+						</div>
+						<div>
+							<input
+								type="email-address"
+								name="email"
+								id="email"
+								placeholder=" Phone Number"
+								className=" bg-transparent border-[0.1rem] border-white rounded-full h-[3rem] w-full placeholder:pl-[1.5rem] placeholder:text-[#E5E4E4]"
+							></input>
 						</div>
 						<div>
 							<input
 								type="password"
 								name="password"
 								id="password"
-								placeholder=" Username"
+								placeholder=" Transaction pin"
 								required
 								className=" bg-transparent border-[0.1rem] border-white rounded-full h-[3rem] w-full placeholder:pl-[1.5rem] placeholder:text-[#E5E4E4]"
 							></input>
@@ -30,14 +42,14 @@ const Login = () => {
 								type="password"
 								name="password"
 								id="password"
-								placeholder=" Password"
+								placeholder=" Confirm Pin"
 								required
 								className=" bg-transparent border-[0.1rem] border-white rounded-full h-[3rem] w-full placeholder:pl-[1.5rem] placeholder:text-[#E5E4E4]"
 							></input>
 						</div>
 						<div className="flex flex-col gap-[1rem]">
 							<button className="bg-white text-black font-normal rounded-full h-[3rem] w-full">
-								Login
+								Complete
 							</button>
 						</div>
 					</form>
@@ -47,4 +59,4 @@ const Login = () => {
 	);
 };
 
-export default Login;
+export default SignUp2;
