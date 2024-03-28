47a52f0e53e59c4ac3541b009daefa1d103886dc
Akshaj05 <122291038+Akshaj05@users.noreply.github.com>
2024-03-28 21:05:11 +0530

    added backend to profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,37 +1,74 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../components/navbar";
 import Profilebg from "../Images/profilebg.png";
+import { db } from "../firebase";
+import {
+  doc as docRef,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+  query,
+  where,
+} from "firebase/firestore";
+import { UserContext } from "../UserContext";
 
 const Profile = () => {
+  const { uid } = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [balance, setBalance] = useState(0);
+
+  const fetchUser = async () => {
+    const docReference = docRef(db, "users", uid);
+    const docSnapshot = await getDoc(docReference);
+
+    if (docSnapshot.exists()) {
+      setName(docSnapshot.data().username);
+      setEmail(docSnapshot.data().email);
+      setBalance(docSnapshot.data().balance);
+    } else {
+      console.log("No such document!");
+    }
+  };
   return (
-    <div className="bg-mainbg w-screen h-screen">
-      <img
-					src={Profilebg}
-					className="absolute h-auto w-auto bottom-0 left-0 z-0 "
-					alt="background"
-				/>{" "}
+    fetchUser(),
+    (
+      <div className="bg-mainbg w-screen h-screen">
+        <img
+          src={Profilebg}
+          className="absolute h-auto w-auto bottom-0 left-0 z-0 "
+          alt="background"
+        />{" "}
         <div className="w-screen h-screen flex flex-col justify-end gap-8 items-center pb-[12rem]">
           <div className="z-10">
-            <p  className=" pr-[14rem] text-white text-[1.2rem] z-10 pb-2">Your Name:</p>
-            <div className="w-[22.75rem] h-[4rem] border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+            <p className=" pr-[14rem] text-white text-[1.2rem] z-10 pb-2">
+              Your Name:
+            </p>
+            <div className="w-[22.75rem] text-white text-[1.2rem] px-5 opacity-60 h-[4rem] border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+              {name}
             </div>
           </div>
           <div className="z-10">
-            <p  className=" pr-[14rem] text-white text-[1.2rem] z-10 pb-2">Your Email:</p>
-            <div className="w-[22.75rem] h-[4rem] border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+            <p className=" pr-[14rem] text-white text-[1.2rem] z-10 pb-2">
+              Your Email:
+            </p>
+            <div className="w-[22.75rem] h-[4rem] text-white text-[1.2rem] px-5 opacity-60 border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+              {email}
             </div>
           </div>
           <div className="z-10">
-            <p  className=" pr-[14rem] text-white text-[1.2rem] z-10 pb-2">Balance:</p>
-            <div className="w-[22.75rem] h-[4rem] border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+            <p className=" pr-[14rem]  text-white text-[1.2rem] z-10 pb-2">
+              Balance:
+            </p>
+            <div className="w-[22.75rem] h-[4rem] text-white text-[1.2rem] px-5 opacity-60 border-lightpurple border-[0.1rem]  bg-lightpurple2 rounded-[1.5rem] flex items-center justify-between z-10 relative">
+              {balance}
             </div>
           </div>
-
-
         </div>
-        
-      <Navbar />
-    </div>
+        <Navbar />
+      </div>
+    )
   );
 };
 
